6f149ed5c8c32ef1206b84618a74378a54b624b7
EdmilsonFlores <edmilsonfloress@gmail.com>
2026-01-21 20:49:11 -0600

    Semana 2
    Trabajo en clase

---
+++ Tienda_Edmilson/src/main/java/Tienda_Edmilson/demo/TiendaEdmilsonApplication.java
@@ -0,0 +1,13 @@
+package Tienda_Edmilson.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaEdmilsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaEdmilsonApplication.class, args);
+	}
+
+}
---
+++ Tienda_Edmilson/src/test/java/Tienda_Edmilson/demo/TiendaEdmilsonApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda_Edmilson.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaEdmilsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
